326c12423394c4f5704be13459b08ac6e163f156
Jose Benitez <pepebenitez12@gmail.com>
2020-12-27 16:32:11 -0500

    Demo project to learn about vue routes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quia alias assumenda earum beatae vero esse, eum architecto nihil praesentium ipsum, distinctio minus consequuntur rerum! Recusandae sed est dolore reiciendis eos laudantium, voluptate illo distinctio nostrum nulla officiis quidem ipsa omnis expedita nisi magni optio excepturi esse quod quae consequatur, ab ea nam! Optio quod hic quas aperiam, maxime assumenda adipisci autem enim eligendi voluptatem eum ducimus libero magnam placeat molestiae nesciunt? Maiores rerum eveniet ipsam voluptatem obcaecati deserunt ullam? Sapiente expedita, eligendi maxime officia ipsum quidem repellendus sequi, iusto quis nihil illum aperiam vero! Quasi enim beatae minima voluptatum?</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores obcaecati necessitatibus vitae dolore, consequatur delectus neque tempora qui, perferendis aspernatur illum esse eaque incidunt, nobis animi accusamus veniam est ab nemo et nisi amet sequi quos. Necessitatibus cumque labore aspernatur omnis animi veniam alias temporibus dolorem eligendi sint? Quam facere ad tenetur. Iste dolor laboriosam dolores. Nisi molestiae, esse, deserunt doloribus laboriosam dolor incidunt quod ea culpa nulla optio distinctio dignissimos, voluptatibus molestias dolore delectus voluptatem earum cum suscipit repellat mollitia explicabo atque? Necessitatibus, minima perferendis? Obcaecati sunt modi neque asperiores, laudantium dignissimos minima eos nam excepturi ut velit labore.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
