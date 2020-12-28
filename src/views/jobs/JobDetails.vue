946af2893d6cfc33c95d4700e861b29d011ababe
Jose Benitez <pepebenitez12@gmail.com>
2020-12-27 20:55:07 -0500

    Conditionally showing data when it is loaded after the fetch request is done

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,7 +1,11 @@
 <template>
-  <h1>{{ job.title }}</h1>
-
-  <p> {{ job.details }}</p>
+    <div v-if="job">
+        <h1>{{ job.title }}</h1>
+        <p> {{ job.details }}</p>
+    </div>
+    <div v-else>
+        <p>Loading job details...</p>
+    </div>
 </template>
 
 <script>
@@ -15,7 +19,7 @@ export default {
         };
     },
     mounted() {
-        fetch('http://localhost:3000/jobs' + this.id)
+        fetch('http://localhost:3000/jobs/' + this.id)
             .then((res) => res.json())
             .then(data => this.job = data)
             .catch(err => console.log(err.message))
