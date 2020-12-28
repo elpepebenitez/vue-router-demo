8f30ccd1a75bc4423f1411c72a1c8a4cd03f33fd
Jose Benitez <pepebenitez12@gmail.com>
2020-12-27 20:40:15 -0500

    Learned about JSON-server package and how to fetch data from external sources

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,6 +1,7 @@
 <template>
-  <h1>Job Details</h1>
-  <p> The job id is {{ id }}</p>
+  <h1>{{ job.title }}</h1>
+
+  <p> {{ job.details }}</p>
 </template>
 
 <script>
@@ -10,8 +11,15 @@ export default {
     data() {
         return {
             // id: this.$route.params.id,
+            job: null,
         };
     },
+    mounted() {
+        fetch('http://localhost:3000/jobs' + this.id)
+            .then((res) => res.json())
+            .then(data => this.job = data)
+            .catch(err => console.log(err.message))
+    }
 }
 </script>
 
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -12,15 +12,18 @@
 export default {
   data() {
       return {
-          jobs: [
-              { title: 'UX Designer', id: 1, details: 'lorem' },
-              { title: 'Web Developer', id: 2, details: 'lorem' },
-              { title: 'Vue Developer', id: 3, details: 'lorem' }
-          ]
+          jobs: [],
       }
   },
   components: {
 
+  }, 
+  mounted() {
+      // Look into asynchronous js and promises
+      fetch('http://localhost:3000/jobs')
+        .then((res) => res.json())
+        .then(data => this.jobs = data)
+        .catch(err => console.log(err.message))
   }
 }
 </script>
